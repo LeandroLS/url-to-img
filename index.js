110c1aa6b89b74c408e471bfb56f1cb6d34c77f2
Leandro <desenvolvedor02@adias.com.br>
2019-09-02 09:58:03 -0300

    Instalando nodemon e o request

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const request = require('request');
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+app.use(express.urlencoded());
 app.get('/', (req, res) => {
     res.render('index');
 });
 app.post('/get-image', (req, res) => {
+    console.log(req.body);
     res.send('vim aqui');
 });
 
