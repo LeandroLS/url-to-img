94b6e6291548bf506d7a653d1fd21499c29dc603
Leandro <desenvolvedor02@adias.com.br>
2019-09-09 12:36:10 -0300

    Adicionando webshot.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const request = require('request');
+const webshot = require('webshot');
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('public'));
@@ -10,8 +10,12 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 app.post('/get-image', (req, res) => {
-    console.log(req.body);
-    res.send('vim aqui');
+    webshot(`${req.body.url}`, `./images/${req.body.url}.jpg`, function(err) {
+        if(err){
+            console.log(err);
+        }
+        // res.download(`${req.body.url}`);
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
