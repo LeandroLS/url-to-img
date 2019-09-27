1c206e2a2dfca27a5e41d7007a14ba20e8416a92
Leandro <desenvolvedor02@adias.com.br>
2019-09-27 15:36:28 -0300

    several css changes

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use(express.urlencoded());
+
 app.get('/', (req, res) => {
     res.render('index');
 });
+
 app.post('/post-image', (req, res) => {
     webshot(`${req.body.url}`, `./public/images/${req.body.url}.jpg`, function(err) {
         img = `${req.body.url}.jpg`;
-        res.render('index', { img, err });
+        if(err) {
+            res.render('index', { err });
+        } else {
+            res.render('index', { img });
+        }
     });
 });
 
